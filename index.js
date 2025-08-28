e779adb3bef52e8a8e0743253a210f153751c58f
Victor-chiemerie <victorchiemerie02@gmail.com>
2025-08-28 21:32:20 +0100

    Start responsive web development

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    header.classList.add("header-with-border");
+    header.classList.remove("header-without-border");
+  } else {
+    header.classList.remove("header-with-border");
+    header.classList.add("header-without-border");
+  }
+});
+
 const screenshots = [
   {
     id: 1,
